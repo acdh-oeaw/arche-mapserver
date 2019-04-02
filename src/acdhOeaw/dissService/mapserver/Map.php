1f63c0bcb3a6badd51034f465bfde1e14726f7a1
Mateusz Żółtak <zozlak@zozlak.org>
2019-04-02 20:43:02 +0200

    Vector layers support

--- src/acdhOeaw/dissService/mapserver/Map.php
+++ src/acdhOeaw/dissService/mapserver/Map.php
@@ -127,8 +127,8 @@ class Map {
      */
     public function refresh(int $keepAlive): bool {
         $refreshed = false;
-        $dataFile = $this->getLocalPath();
-        $mapFile = $dataFile . '.map';
+        $dataFile  = $this->getLocalPath();
+        $mapFile   = $dataFile . '.map';
 
         $d = (new DateTime('now'))->diff(DateTime::createFromFormat('Y-m-d H:i:s', $this->checkDate));
         $d = $d->format('%a') * 24 * 3600 + $d->h * 3600 + $d->i * 60 + $d->s + $d->f;
@@ -149,11 +149,13 @@ class Map {
 
         if (!file_exists($mapFile)) {
             $tmpl      = file_get_contents(self::$templates[$this->type]);
-            list($xmin, $ymin, $xmax, $ymax) = $this->getExtent();
-            $tmpl      = str_replace('%X_MIN%', $xmin, $tmpl);
-            $tmpl      = str_replace('%X_MAX%', $xmax, $tmpl);
-            $tmpl      = str_replace('%Y_MIN%', $ymin, $tmpl);
-            $tmpl      = str_replace('%Y_MAX%', $ymax, $tmpl);
+            $d         = $this->getGeodata();
+            $tmpl      = str_replace('%X_MIN%', $d->xmin, $tmpl);
+            $tmpl      = str_replace('%X_MAX%', $d->xmax, $tmpl);
+            $tmpl      = str_replace('%Y_MIN%', $d->ymin, $tmpl);
+            $tmpl      = str_replace('%Y_MAX%', $d->ymax, $tmpl);
+            $tmpl      = str_replace('%IDCOL%', $d->idCol, $tmpl);
+            $tmpl      = str_replace('%SRID%', $d->srid, $tmpl);
             $tmpl      = str_replace('%ID%', $this->id, $tmpl);
             $tmpl      = str_replace('%NAME%', $this->getName(), $tmpl);
             $tmpl      = str_replace('%FILE%', $dataFile, $tmpl);
@@ -180,11 +182,12 @@ class Map {
     private function getRemoteFileInfo(): RemoteFileInfo {
         if ($this->remoteFileInfo === null) {
             // find the real resource URI
-            $client = new Client(['allow_redirects' => ['track_redirects' => true], 'verify' => false]);
-            $response = $client->send(new Request('HEAD', $this->archeId));
+            $client    = new Client(['allow_redirects' => ['track_redirects' => true],
+                'verify'          => false]);
+            $response  = $client->send(new Request('HEAD', $this->archeId));
             $redirects = array_merge([$this->archeId], $response->getHeader('X-Guzzle-Redirect-History'));
-            $url = array_pop($redirects);
-            $metaUrl = $url . '/fcr:metadata';
+            $url       = array_pop($redirects);
+            $metaUrl   = $url . '/fcr:metadata';
 
             // fetch metadata
             $graph = new Graph();
@@ -231,12 +234,71 @@ class Map {
     }
 
     /**
-     * Returns extent of an already cached map.
+     * Returns various geodata of an already cached map.
      * 
-     * Returns coordinates in the xmin, ymin, xmax, ymax order.
-     * @return array
+     * @return \stdClass
      */
-    public function getExtent(): array {
+    public function getGeodata(): stdClass {
+        $d = (object) [
+                'xmin'  => -180,
+                'xmax'  => 180,
+                'ymin'  => -90,
+                'ymax'  => 90,
+                'idCol' => 'id',
+                'srid'  => 4326
+        ];
+
+        switch ($this->type) {
+            case 'raster':
+                $d = $this->getRasterGeodata($d);
+                break;
+            case 'vector':
+                $d = $this->getVectorGeodata($d);
+                break;
+        }
+        return $d;
+    }
+
+    private function getVectorGeodata(stdClass $d): stdClass {
+        $output = [];
+        $cmd    = 'ogrinfo -nomd -so ' . escapeshellarg($this->getLocalPath());
+        exec($cmd, $output);
+        foreach ($output as $l) {
+            if (substr($l, 0, 2) === '1:') {
+                $layer = preg_replace('/^1: ([^ ]+)( .*$)?/', '\\1', $l);
+                break;
+            }
+        }
+        $cmd = 'ogrinfo -nomd -so ' . escapeshellarg($this->getLocalPath()) . ' ' . escapeshellarg($layer);
+        exec($cmd, $output);
+
+        $wktFlag = 0;
+        $idCol   = '';
+        foreach ($output as $l) {
+            if (substr($l, 0, 7) === 'Extent:') {
+                $ll      = explode(' ', preg_replace('/  +/', ' ', trim(preg_replace('/[^0-9. ]/', '', $l))));
+                $d->xmin = $ll[0];
+                $d->xmax = $ll[2];
+                $d->ymin = $ll[1];
+                $d->ymax = $ll[3];
+            }
+            if (substr($l, 0, 14) === 'Layer SRS WKT:') {
+                $wktFlag = 1;
+            }
+            if ($wktFlag === 1 && substr($l, 0, 1) !== ' ') {
+                $wktFlag = 2;
+                $idCol   = preg_replace('/: .*$/', '', $l);
+            }
+            if ($wktFlag === 2 && preg_replace('/: .*$/', '', $l) === 'id') {
+                $idCol   = 'id';
+                $wktFlag = 3;
+            }
+        }
+        $d->idCol = $idCol;
+        return $d;
+    }
+
+    private function getRasterGeodata(stdClass $d): stdClass {
         $output = [];
         $cmd    = 'gdalinfo ' . escapeshellarg($this->getLocalPath());
         exec($cmd, $output);
@@ -250,13 +312,13 @@ class Map {
         }
         $max = $output[$n];
 
-        $min  = explode(' ', preg_replace('/^.*[(] *([0-9.]+), *([0-9.]+)[)].*$/', '\\1 \\2', $min));
-        $max  = explode(' ', preg_replace('/^.*[(] *([0-9.]+), *([0-9.]+)[)].*$/', '\\1 \\2', $max));
-        $xmin = min($min[0], $max[0]);
-        $xmax = max($min[0], $max[0]);
-        $ymin = min($min[1], $max[1]);
-        $ymax = max($min[1], $max[1]);
-        return [$xmin, $ymin, $xmax, $ymax];
+        $min     = explode(' ', preg_replace('/^.*[(] *([0-9.]+), *([0-9.]+)[)].*$/', '\\1 \\2', $min));
+        $max     = explode(' ', preg_replace('/^.*[(] *([0-9.]+), *([0-9.]+)[)].*$/', '\\1 \\2', $max));
+        $d->xmin = min($min[0], $max[0]);
+        $d->xmax = max($min[0], $max[0]);
+        $d->ymin = min($min[1], $max[1]);
+        $d->ymax = max($min[1], $max[1]);
+        return $d;
     }
 
     /**
