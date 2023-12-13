e455b38d5f177e126872ba5ebfbde3003ee99329
Mateusz Żółtak <zozlak@zozlak.org>
2023-12-13 17:21:54 +0100

    build/arche/download.sh: include the test map

--- build/arche/download.sh
+++ build/arche/download.sh
@@ -3,5 +3,7 @@ curl https://shared.acdh.oeaw.ac.at/histogis/czoernig.map > /data/czoernig.map
 curl https://shared.acdh.oeaw.ac.at/histogis/czoernig.tif > /data/czoernig.tif
 curl https://shared.acdh.oeaw.ac.at/histogis/tirol.map > /data/tirol.map
 curl https://shared.acdh.oeaw.ac.at/histogis/tirol.tif > /data/tirol.tif
+curl https://shared.acdh.oeaw.ac.at/histogis/test.map > /data/test.map
+curl https://shared.acdh.oeaw.ac.at/histogis/test.tif > /data/test.tif
 
 chown www-data:www-data /data/*
